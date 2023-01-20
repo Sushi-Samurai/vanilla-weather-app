4ec3437cafcb504055beeedce6d6e40d02552120
Sushi-Samurai <122423970+Sushi-Samurai@users.noreply.github.com>
2023-01-19 18:43:07 -0800

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = response.data.condition.icon;
+}
+
 let apiKey = "6b0f0fb3t5ba9cb4bo7e315a113434e7";
+let city = "Sacramento";
+let units = "imperial";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
